35086a7f33adfb2e556a4650f1674d42663240e9
gluka <luka.gojakovic7@gmail.com>
2023-09-28 13:34:24 +0200

    .

--- src/main/java/cgtonboardingactorsmain/actorsApi/mapper/Mapper.java
+++ src/main/java/cgtonboardingactorsmain/actorsApi/mapper/Mapper.java
@@ -34,7 +34,6 @@ public class Mapper {
         actorDto.setGender(actor.getGender());
         actorDto.setFullName(actor.getFullName());
         actorDto.setDateOfBirth(actor.getDateOfBirth());
-//        actorDto.setActorImage(actor.getImageName());
         actorDto.setParents(actor.getParents());
         actorDto.setBiography(actor.getBiography());
         actorDto.setSpouse(actor.getSpouse());
@@ -47,13 +46,13 @@ public class Mapper {
         actorDto.setHref(linkTo(ActorsController.class).slash(actorDto.getId()).toString());
 
         File file = new File(actor.getImageName());
+        actorDto.setImageName(actor.getImageName());
 
         try (FileInputStream fis = new FileInputStream(file)){
             byte[] fileContent = new byte[(int) file.length()];
             fis.read(fileContent);
             String encodedString = Base64.getEncoder().encodeToString(fileContent);
             actorDto.setActorImage("<base64 image>,"+encodedString);
-            actorDto.setImageName(actor.getImageName());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -72,8 +71,8 @@ public class Mapper {
         actor.setDateOfBirth(actorDto.getDateOfBirth());
         actor.setSpouse(actorDto.getSpouse());
 
-        String[] name = actorDto.getActorImage().split(",");
-        String dataUri = name[1];
+        //String[] name = actorDto.getActorImage().split(",");
+        String dataUri = actorDto.getActorImage();
         Random random = new Random();
         try {
             byte[] decodedBytes = Base64.getDecoder().decode(dataUri);
